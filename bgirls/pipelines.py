ce0b85dddd9e85144d937f64c0b895ec78e73a6e
hxx0215 <hxx0215@126.com>
2015-11-15 16:07:17 +0800

    update log

--- bgirls/pipelines.py
+++ bgirls/pipelines.py
@@ -11,10 +11,19 @@ from scrapy.exceptions import DropItem
 import logging
 
 class MongoDBPipeline(object):
+    newItem = 0
+    updatedItem = 0
     def __init__(self):
         connection = pymongo.MongoClient(settings['MONGODB_SERVER'],settings['MONGODB_PORT'])
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def open_spider(self,spider):
+        print "begin"
+
+    def close_spider(self,spider):
+        print "%d newItem %d updated" % (self.newItem,self.updatedItem)
+
     def process_item(self, item, spider):
         valid = True
         for data in item:
@@ -23,5 +32,9 @@ class MongoDBPipeline(object):
                 raise DropItem("Missing {0}".format(data))
         if valid:
             res = self.collection.replace_one({"_id" : item["_id"],},dict(item),True)
-            logging.log(logging.DEBUG,"bgirl add to MongoDB")
+            if res.upserted_id:
+                self.newItem += 1
+                logging.log(logging.DEBUG,"a girl item insert")
+            else:
+                self.updatedItem += 1
         return item
